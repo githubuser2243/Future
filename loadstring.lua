163d119973f4059175dca02be928e6852ec20186
githubuser2243 <105172547+githubuser2243@users.noreply.github.com>
2022-10-17 16:38:31 +0100

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/joeengo/Future/main/Initiate.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/githubuser2243/Future/main/Initiate.lua"))()
